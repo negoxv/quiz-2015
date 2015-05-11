5cf155145b4849eeca53db379552afaf47fd8570
negoxv <negoxv@gmail.com>
2015-05-11 17:31:10 +0200

    version lab

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,11 @@
+//get /quizes/question
+exports.question=function(req, res){
+	res.render('quizes/question', {pregunta: 'capital de Italia'});
+};
+exports.answer=function(req, res){
+	if(req.query.respuesta === 'Roma'){
+		res.render('quizes/answer', {respuesta: 'Correcto'});
+	}else{
+		res.render('quizes/answer', {respuesta: 'Incorrecto'});
+	}
+};
