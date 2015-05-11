c348dfbafc3a2f660b7557136e3b30eba8f433da
negoxv <negoxv@gmail.com>
2015-05-11 18:32:10 +0200

    version 11m.1832

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,11 +1,46 @@
-//get /quizes/question
+//get /quizes/questio
+var models = require('../models/models.js');
+// exports.load=function(req, res, next, quizId){
+// 	models.Quiz.find(quizId).then(function(quiz){
+// 		if(quiz){
+// 			req.quiz = quiz;
+// 			next();
+// 		}else{next(new Error('No existe quizId=' + quizId));}
+
+// }).catch(function(error){next(error);});
+// };
+// exports.show=function(req, res){
+// 	models.Quiz.find(req.params.quizId).then(function(quiz){
+// 	res.render('quizes/show', {quiz: quiz});
+// }
+// )
+// };
+// exports.answer=function(req, res){
+// 	models.Quiz.find(reqId).then(function(quiz){
+// 		if(req.query.respuesta === quiz.respuesta){
+// 		res.render('quizes/answer', {quiz: quiz, respuesta: 'Correcto'});
+// 	}else{
+// 		res.render('quizes/answer', {quiz: quiz, respuesta: 'Incorrecto'});
+// 	}
+// 	})
+	
+// };
+// exports.index=function(req, res){
+// 	models.Quiz.findAll().then(function(quizes){
+// 	res.render('quizes/index.ejs', {quizes: quizes});
+// }).catch(function(error){next(error);});
+// };
 exports.question=function(req, res){
-	res.render('quizes/question', {pregunta: 'capital de Italia'});
+	models.Quiz.findAll().then(function(quiz){
+	res.render('quizes/question', {pregunta: quiz[0].pregunta});
+});//).catch(function(error){next(error);});
 };
 exports.answer=function(req, res){
-	if(req.query.respuesta === 'Roma'){
-		res.render('quizes/answer', {respuesta: 'Correcto'});
-	}else{
-		res.render('quizes/answer', {respuesta: 'Incorrecto'});
-	}
+	models.Quiz.findAll().then(function(quiz){
+if(req.query.respuesta === quiz[0].respuesta){
+		res.render('quizes/answer', { respuesta: 'Correcto'});
+ 	}else{
+ 		res.render('quizes/answer', { respuesta: 'Incorrecto'});
+ 	}
+});//).catch(function(error){next(error);});
 };
